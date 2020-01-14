3fa91f5a98fc404b53c7a930ff90d33bed2187c1
Michał Szymecki <szymsonik@gmail.com>
2020-01-14 21:06:20 +0100

    odkrycie że ctrl + alt + k robi commit i push

--- src/Homework1.java
+++ src/Homework1.java
@@ -4,5 +4,6 @@ public class Homework1 {
         System.out.println(" i \ncommitowania");
         System.out.println("Oto nowa linijka kodu");
         System.out.println("i jeszcze jedna, a co");
+        System.out.println("właśnie odkryłem fajną rzecz");
     }
 }
