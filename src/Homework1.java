d22e6ac51099607d1a172b7cc43dca0163fb6611
Michał Szymecki <szymsonik@gmail.com>
2020-01-14 20:58:21 +0100

    kilka nowych linijek

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,5 +1,6 @@
 public class Homework1 {
     public static void main(String[] args) {
         System.out.println("Uczę \nsię \nprogramowania");
+        System.out.println(" i \ncommitowania");
     }
 }
