92346539375c98ebdaae35dc1924560f3f7294b8
Michał Szymecki <szymsonik@gmail.com>
2020-01-14 21:02:23 +0100

    trzecia nowa linijka

--- src/Homework1.java
+++ src/Homework1.java
@@ -2,5 +2,6 @@ public class Homework1 {
     public static void main(String[] args) {
         System.out.println("Uczę \nsię \nprogramowania");
         System.out.println(" i \ncommitowania");
+        System.out.println("Oto nowa linijka kodu");
     }
 }
