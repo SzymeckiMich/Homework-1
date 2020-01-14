0b53b369b2483bb509f19729213e3c3614e876a8
Michał Szymecki <szymsonik@gmail.com>
2020-01-14 21:08:27 +0100

    poprawka błędu

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,7 +1,7 @@
 public class Homework1 {
     public static void main(String[] args) {
         System.out.println("Uczę \nsię \nprogramowania");
-        System.out.println(" i \ncommitowania");
+        System.out.println("i \ncommitowania");
         System.out.println("Oto nowa linijka kodu");
         System.out.println("i jeszcze jedna, a co");
         System.out.println("właśnie odkryłem fajną rzecz");
